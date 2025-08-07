e0a54f92725e5f07799cd13fff8ae1bb67fc1610
Raffa <165601931+RafaelGogge@users.noreply.github.com>
2025-08-07 10:44:38 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  swcMinify: false,
+  experimental: {},
 };
 
 export default nextConfig;
